a37aac2f121735d54eb8824274d4c854227b9cac
Ian Oliveira <92691847+IanOliveiraSilva@users.noreply.github.com>
2022-02-03 19:53:41 -0300

    Update Hash-Project-1.rb

--- Hash-Project-1.rb
+++ Hash-Project-1.rb
@@ -1,3 +1,5 @@
+#Description:  a Hash-type collection that allows the user to create three elements informing the key and value. At the end of the program for each of these elements prints the sentence “One of the keys is **** and its value is ****”
+
 pais_capital = Hash.new
 i = 1
 v = 0
