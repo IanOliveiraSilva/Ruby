eaaa0e96629f973cd2c6bad191eb52ec4dc39659
Vesper03 <xxndtry33@gmail.com>
2022-02-04 16:52:48 -0300

    CPF PROJECT

---
+++ Gem-Cpf-Project.rb
@@ -0,0 +1,15 @@
+#DESCRIPTION: using the cpf_cnpj gem to create a program that takes as input a cpf number and in a method checks if this number is valid.
+
+#Documentation link:
+#https://github.com/fnando/cpf_cnpj
+
+require 'Cpf_cnpj'
+
+print "Digite o CPF: "
+CPF_Check = gets.chomp
+
+if CPF.valid?(CPF_Check)
+    puts "O CPF é valido"
+else
+    puts "O CPF não é valido"
+end
